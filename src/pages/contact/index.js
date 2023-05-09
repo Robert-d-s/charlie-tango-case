60c1243e3dbc551cb82e6656cfa1a5490958a613
Robert Stoica <siriussrd@gmail.com>
2023-05-09 21:12:24 +0200

    vercel

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -136,6 +136,7 @@ export default function Contact() {
                 <div className={styles.desc}>
                   <p>Description</p>
                   <p>{buyer.description}</p>
+                  {/* <p>{buyer.description}</p> */}
                 </div>
               </div>
             ))}
